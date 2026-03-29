1f8a9c0cd6098a311f1b2664167f530e650f94db
Kacper Kwapisz <kacper.kwapisz@mail.com>
2026-03-29 21:40:25 +0200

    chore: bump agent version to 0.2.0

--- internal/heartbeat/heartbeat.go
+++ internal/heartbeat/heartbeat.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stackedapp/stacked/agent/internal/client"
 )
 
-const Version = "0.1.0"
+const Version = "0.2.0"
 
 // systemInfo holds static machine info collected once at startup.
 type systemInfo struct {
